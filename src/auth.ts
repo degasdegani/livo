9fb53e6ba80d48b162a54a69eb52c5f750399c88
Eduardo Degani <eduardodegani97@gmail.com>
2026-06-04 02:44:26 -0300

    teste remove ok

--- src/auth.ts
+++ src/auth.ts
@@ -72,10 +72,10 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
 
   // Disparado quando um novo usuário é criado (Google OAuth)
   //events: {
-   // async createUser({ user }) {
-     // if (user.email && user.name) {
-       // await sendWelcomeEmail(user.email, user.name);
-      //}
-    //},
+  // async createUser({ user }) {
+  // if (user.email && user.name) {
+  // await sendWelcomeEmail(user.email, user.name);
+  //}
   //},
-//});
+  //},
+});
