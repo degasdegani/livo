de9192a5a695909b1c14a5b7f633bb1080acd071
Eduardo Degani <eduardodegani97@gmail.com>
2026-06-04 02:24:41 -0300

    new test

--- src/auth.ts
+++ src/auth.ts
@@ -16,11 +16,6 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
 
   session: { strategy: "jwt" },
 
-  pages: {
-    signIn: "/login",
-    error: "/login",
-  },
-
   providers: [
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID!,
